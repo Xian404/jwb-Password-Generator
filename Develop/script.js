b3cedcb7172ede258bee99d4ebafe891e10a906c
Jerome Beverly <jeromew.beverly@gmail.com>
2022-04-21 23:38:59 -0500

    adding variables for functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,18 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// setting var for lowercase letters
+let alphaLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+// setting var for uppercase letters
+let alphaUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+// setting var for single digit numbers 0 thru 9
+let numberChara = ['0','1','2','3','4','5','6','7','8','9']
+// setting var for special characters
+let specialChara = ['!','@','#','$','%','*','?','/']
+
+
 // Setting first criteria of character length for password
-
-
-
-function () {
-  var answerA = prompt ('How long would you like your password to be? Must be greater than or equal to 8 and less than or equal to 128 characters', 16)
-  If (answerA == Number && >= 8 && <= 128) {
-    
-  } 
-
-}
 
 
 // Write password to the #password input
