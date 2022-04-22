e53fd30633da59853d02122b32b8bd67554bf70a
Jerome Beverly <jeromew.beverly@gmail.com>
2022-04-21 23:11:30 -0500

    I am confusion - trying to make functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Setting first criteria of character length for password
+
+
+
+function () {
+  var answerA = prompt ('How long would you like your password to be? Must be greater than or equal to 8 and less than or equal to 128 characters', 16)
+  If (answerA == Number && >= 8 && <= 128) {
+    
+  } 
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
